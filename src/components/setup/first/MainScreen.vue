15505d5bd9bb042a00eeb2c8095b284d662c561d
CredoRevolution <sadistik871@mail.ru>
2024-05-28 15:23:01 +0300

    improovements :_)

--- src/components/setup/first/MainScreen.vue
+++ src/components/setup/first/MainScreen.vue
@@ -62,21 +62,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@function rem($px) {
+  @return ($px / 16px) + rem;
+}
 .main-screen {
   max-height: 95%;
   height: 95%;
   max-width: 90%;
   background: #fff;
-  border-radius: 43px;
+  border-radius: rem(43px);
   margin: 0 auto;
-  padding: 34px 40px;
+  padding: rem(34px) rem(40px);
   &__progress {
     width: 95%;
-    margin-top: 11px;
-    margin-bottom: 17px;
-    height: 4px;
+    margin-top: rem(11px);
+    margin-bottom: rem(17px);
+    height: rem(4px);
     background: #f5f5f8;
-    border-radius: 13px;
+    border-radius: rem(13px);
     position: relative;
 
     .progress {
@@ -87,24 +90,24 @@ export default {
       width: 1%;
       height: 100%;
       background: #14121f;
-      border-radius: 13px;
+      border-radius: rem(13px);
     }
   }
   &__title {
-    line-height: 48px;
-    font-size: 40px;
+    line-height: rem(48px);
+    font-size: rem(40px);
     font-weight: 700;
 
     color: #14121f;
-    margin-bottom: 17px;
+    margin-bottom: rem(17px);
   }
 
   &__text {
-    line-height: 21px;
-    font-size: 17px;
+    line-height: rem(21px);
+    font-size: rem(17px);
     font-weight: 700;
     color: #86868b;
-    margin-bottom: 50px;
+    margin-bottom: rem(50px);
   }
 
   &__list {
@@ -112,19 +115,19 @@ export default {
     justify-content: space-between;
     align-items: center;
     flex-wrap: wrap;
-    row-gap: 17px;
-    margin-bottom: 31px;
+    row-gap: rem(17px);
+    margin-bottom: rem(31px);
   }
 
   &__btn {
     font-weight: 700;
-    font-size: 24px;
-    line-height: 29px;
+    font-size: rem(24px);
+    line-height: rem(29px);
     letter-spacing: 0.02em;
     color: #fff;
-    padding: 17px 31px;
+    padding: rem(17px) rem(31px);
     background: #0071e2;
-    border-radius: 999px;
+    border-radius: rem(999px);
     text-decoration: none;
     transition: all 0.3s ease;
     box-sizing: content-box;
